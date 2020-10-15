ab480d004e46105ae805a41aa28910ae08925f2a
PieroCastillo <39932324+PieroCastillo@users.noreply.github.com>
2020-10-14 23:51:41 -0500

    Update Tests

--- Tests/NugetTest/MainWindow.axaml.cs
+++ Tests/NugetTest/MainWindow.axaml.cs
@@ -1,11 +1,13 @@
-﻿using Avalonia;
+﻿using Aura.UI.Controls;
+using Avalonia;
 using Avalonia.Controls;
 using Avalonia.Markup.Xaml;
 
 namespace NugetTest
 {
-    public class MainWindow : Window
+    public class MainWindow : ContentWindow
     {
+        Border drag;
         public MainWindow()
         {
             InitializeComponent();
@@ -14,6 +16,13 @@ namespace NugetTest
         private void InitializeComponent()
         {
             AvaloniaXamlLoader.Load(this);
+            drag = this.Find<Border>("drag");
+            drag.PointerPressed += Drag_PointerPressed;
+        }
+
+        private void Drag_PointerPressed(object sender, Avalonia.Input.PointerPressedEventArgs e)
+        {
+            this.BeginMoveDrag(e);
         }
     }
 }
---
+++ Tests/UI.Tests/Views/MusicView.axaml.cs
@@ -0,0 +1,19 @@
+﻿using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace UI.Tests.Views
+{
+    public class MusicView : UserControl
+    {
+        public MusicView()
+        {
+            this.InitializeComponent();
+        }
+
+        private void InitializeComponent()
+        {
+            AvaloniaXamlLoader.Load(this);
+        }
+    }
+}
