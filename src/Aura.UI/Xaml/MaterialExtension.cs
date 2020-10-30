b6d9d0bce231eaa557a88936eb1af707505c399d
PieroCastillo <39932324+PieroCastillo@users.noreply.github.com>
2020-10-30 00:29:21 -0500

    working in new features

---
+++ src/Aura.UI/Xaml/MaterialExtension.cs
@@ -0,0 +1,26 @@
+﻿using Avalonia.Markup.Xaml;
+using Avalonia.Media;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Aura.UI.Xaml
+{
+    public class MaterialExtension : MarkupExtension
+    {
+        public override object ProvideValue(IServiceProvider serviceProvider)
+        {
+            return new ExperimentalAcrylicMaterial 
+            { 
+                TintColor = TintColor,
+                FallbackColor = FallBackColor,
+                TintOpacity = TintOpacity,
+                MaterialOpacity = MaterialOpacity
+            };
+        }
+        public Color TintColor { get; set; }
+        public Color FallBackColor { get; set; }
+        public double TintOpacity { get; set; }
+        public double MaterialOpacity { get; set; }
+    }
+}
