70ac55ab36f194c31dd5df669042cb5acb744a70
PieroCastillo <39932324+PieroCastillo@users.noreply.github.com>
2020-10-30 00:30:00 -0500

    updated to avalonia 0.10 preview 6

--- src/Aura.UI/Assembly.cs
+++ src/Aura.UI/Assembly.cs
@@ -11,5 +11,5 @@ using Avalonia.Metadata;
 [assembly: XmlnsDefinition("https://github.com/avaloniaui", "Aura.UI.Controls.Navigation")]
 [assembly: XmlnsDefinition("https://github.com/avaloniaui", "Avalonia.Xaml.Interactivity")]
 [assembly: XmlnsDefinition("https://github.com/avaloniaui", "Avalonia.Xaml.Interactions")]
-//[assembly: XmlnsDefinition("https://github.com/avaloniaui", "Aura.UI.Xaml")]
+[assembly: XmlnsDefinition("https://github.com/avaloniaui", "Aura.UI.Xaml")]
 //[assembly: XmlnsDefinition("https://github.com/avaloniaui", "Aura.UI.Controls.Status")]
