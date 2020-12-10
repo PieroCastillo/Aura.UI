1bea2a84d0211018c9e6d1fcd2a8b8ec5b9bd7da
PieroCastillo <39932324+PieroCastillo@users.noreply.github.com>
2020-12-10 17:49:06 -0500

    MVVM Test

---
+++ UI.MVVM/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,24 @@
+﻿using Avalonia.Collections;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Aura.UI.Controls;
+
+namespace UI.MVVM.ViewModels
+{
+    public class MainWindowViewModel : ViewModelBase
+    {
+        public string Greeting => "Welcome to Avalonia!";
+
+        public IEnumerable<object> items
+        {
+            get => new AvaloniaList<object>
+            {
+                new AuraTabItem{ Header = "Tab 1" },
+                new AuraTabItem{ Header = "Tab 2" },
+                new AuraTabItem{ Header = "Tab 3" },
+                new AuraTabItem{ Header = "Tab 4" }
+            };
+        }
+    }
+}
---
+++ UI.MVVM/ViewModels/ViewModelBase.cs
@@ -0,0 +1,11 @@
+﻿using ReactiveUI;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UI.MVVM.ViewModels
+{
+    public class ViewModelBase : ReactiveObject
+    {
+    }
+}
