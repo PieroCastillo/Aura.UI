b659ea8610702953d91351ace34faf36784a1e1a
PieroCastillo <39932324+PieroCastillo@users.noreply.github.com>
2020-12-10 17:50:23 -0500

    recomended configuration: Aura.UI 0.1.3-preview9 and Aura.UI.Dragging 0.1.3-preview11

--- Tests/NugetTest/Program.cs
+++ Tests/NugetTest/Program.cs
@@ -15,6 +15,6 @@ namespace NugetTest
         public static AppBuilder BuildAvaloniaApp()
             => AppBuilder.Configure<App>()
                 .UsePlatformDetect()
-                .LogToDebug();
+                .LogToTrace();
     }
 }
