e9f417097be1bda315c68623e0b51adf95fcde32
PieroCastillo <39932324+PieroCastillo@users.noreply.github.com>
2020-12-11 00:33:50 -0500

    ContentDialogService

---
+++ src/Aura.UI/Services/ContentDialogService.cs
@@ -0,0 +1,44 @@
+﻿using Avalonia.Controls;
+using Avalonia.Controls.Primitives;
+using Avalonia.Threading;
+using System.Threading.Tasks;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Diagnostics;
+
+namespace Aura.UI.Services
+{
+    internal static class ContentDialogService
+    {
+        internal  static void ShowDialogOn(WindowBase window, Control dialog)
+        {
+           Dispatcher.UIThread.Post(() =>
+           {
+               dialog.Width = window.Bounds.Width;
+               dialog.Height = window.Bounds.Height;
+               window.PropertyChanged += (s,e) =>
+               {
+                   dialog.Width = window.Bounds.Width;
+                   dialog.Height = window.Bounds.Height;
+               };
+               var layer = OverlayLayer.GetOverlayLayer(window);
+               layer.Children.Add(dialog);
+               Debug.WriteLine("dialog added");
+           });
+        }
+
+        internal static void CloseDialogOn(WindowBase window, Control dialog)
+        {
+           Dispatcher.UIThread.Post(() => 
+           {
+               var layer = OverlayLayer.GetOverlayLayer(window);
+               if (layer.Children.Contains(dialog))
+               {
+                   layer.Children.Remove(dialog);
+               }
+               Debug.WriteLine("dialog closed");
+           });
+        }
+    } 
+}
