7e0dbe41d4efc1320093a8174907b916e7b2d949
PieroCastillo <39932324+PieroCastillo@users.noreply.github.com>
2020-12-21 12:36:40 -0500

    working on new and modern ColorPicker

---
+++ src/Aura.UI/Controls/Colouring/ColorSquare/ColorSquare.Properties.cs
@@ -0,0 +1,23 @@
+﻿using Avalonia;
+using Avalonia.Media;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Aura.UI.Controls.Colouring
+{
+    public partial class ColorSquare
+    {
+        private Color _colortorender = Colors.Red;
+        public Color ColorToRender
+        {
+            get => _colortorender;
+            set => SetAndRaise(ColorToRenderProperty, ref _colortorender, value);
+        }
+        public readonly static DirectProperty<ColorSquare, Color> ColorToRenderProperty =
+            AvaloniaProperty.RegisterDirect<ColorSquare, Color>(
+                nameof(ColorToRender),
+                o => o.ColorToRender,
+                (o,v) => o.ColorToRender = v);
+    }
+}
---
+++ src/Aura.UI/Controls/Colouring/ColorSquare/ColorSquare.cs
@@ -0,0 +1,18 @@
+﻿using Aura.UI.Rendering;
+using Avalonia.Controls;
+using Avalonia.Controls.Primitives;
+using Avalonia.Media;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Aura.UI.Controls.Colouring
+{
+    public partial class ColorSquare : Control
+    {
+        public override void Render(DrawingContext context)
+        {
+            context.Custom(new ColorSquareRender(Bounds, null, ColorToRender, Colors.LightGray));
+        }
+    }
+}
