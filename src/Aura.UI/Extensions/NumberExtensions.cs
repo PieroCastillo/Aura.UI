63daa5cf09b68687f503787b5c430b20314d9adb
PieroCastillo <39932324+PieroCastillo@users.noreply.github.com>
2020-12-29 11:09:40 -0500

    add new extensions

---
+++ src/Aura.UI/Extensions/NumberExtensions.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Aura.UI.Extensions
+{
+    public static class NumberExtensions
+    {
+        public static byte ToByte(this double d) => (byte)d;
+        public static float ToFloat(this double d) => (float)d;
+    }
+}
