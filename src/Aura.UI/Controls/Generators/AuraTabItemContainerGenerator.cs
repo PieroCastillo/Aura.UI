903040c8e59ad18b778a2780b079a773aaeea4d8
PieroCastillo <39932324+PieroCastillo@users.noreply.github.com>
2021-01-02 00:24:44 -0500

    generators for mvvm is now

---
+++ src/Aura.UI/Controls/Generators/AuraTabItemContainerGenerator.cs
@@ -0,0 +1,54 @@
+﻿using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Controls.Generators;
+using Avalonia.Controls.Primitives;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Aura.UI.Controls.Generators
+{
+    public class AuraTabItemContainerGenerator : ItemContainerGenerator<AuraTabItem>
+    {
+        public AuraTabItemContainerGenerator(AuraTabView owner) : base(owner, ContentControl.ContentProperty, ContentControl.ContentTemplateProperty)
+        {
+            Owner = owner;
+        }
+
+        public new AuraTabView Owner { get; }
+
+        protected override IControl CreateContainer(object item)
+        {
+            var tabItem = (AuraTabItem)base.CreateContainer(item);
+
+            tabItem[~TabControl.TabStripPlacementProperty] = Owner[~TabControl.TabStripPlacementProperty];
+
+            if (tabItem.HeaderTemplate == null)
+            {
+                tabItem[~HeaderedContentControl.HeaderTemplateProperty] = Owner[~ItemsControl.ItemTemplateProperty];
+            }
+
+            if (tabItem.Header == null)
+            {
+                if (item is IHeadered headered)
+                {
+                    tabItem.Header = headered.Header;
+                }
+                else
+                {
+                    if (!(tabItem.DataContext is IControl))
+                    {
+                        tabItem.Header = tabItem.DataContext;
+                    }
+                }
+            }
+
+            if (!(tabItem.Content is IControl))
+            {
+                tabItem[~ContentControl.ContentTemplateProperty] = Owner[~AuraTabView.ContentTemplateProperty];
+            }
+
+            return tabItem;
+        }
+    }
+}
