43eead437821c7a8383407d6a8747e374ddfa52c
PieroCastillo <39932324+PieroCastillo@users.noreply.github.com>
2021-01-02 00:27:56 -0500

    Bindable BoxShadow

--- src/Aura.UI/Xaml/AcrylicMaterialExtension.cs
+++ src/Aura.UI/Xaml/AcrylicMaterialExtension.cs
@@ -1,4 +1,5 @@
-﻿using Avalonia.Markup.Xaml;
+﻿using Avalonia;
+using Avalonia.Markup.Xaml;
 using Avalonia.Media;
 using System;
 using System.Collections.Generic;
@@ -6,9 +7,9 @@ using System.Text;
 
 namespace Aura.UI.Xaml
 {
-    public class AcrylicMaterialExtension : MarkupExtension
+    public class AcrylicMaterialExtension : AvaloniaObject
     {
-        public override object ProvideValue(IServiceProvider serviceProvider)
+        public object ProvideValue(IServiceProvider serviceProvider)
         {
             return new ExperimentalAcrylicMaterial
             {
@@ -19,11 +20,41 @@ namespace Aura.UI.Xaml
                 BackgroundSource = this.AcrylicBackgroundSource
             };
         }
-        public Color TintColor { get; set; }
-        public Color FallBackColor { get; set; }
-        public double TintOpacity { get; set; }
-        public double MaterialOpacity { get; set; }
+        public Color TintColor
+        {
+            get => GetValue(TintColorProperty);
+            set => SetValue(TintColorProperty, value);
+        }
+        public readonly static StyledProperty<Color> TintColorProperty =
+            AvaloniaProperty.Register<AcrylicMaterialExtension, Color>(nameof(TintColor));
+        public Color FallBackColor
+        {
+            get => GetValue(FallBackColorProperty);
+            set => SetValue(FallBackColorProperty, value);
+        }
+        public readonly static StyledProperty<Color> FallBackColorProperty =
+            AvaloniaProperty.Register<AcrylicMaterialExtension, Color>(nameof(FallBackColor));
+        public double TintOpacity
+        {
+            get => GetValue(TintOpacityProperty);
+            set => SetValue(TintOpacityProperty, value);
+        }
+        public readonly static StyledProperty<double> TintOpacityProperty =
+            AvaloniaProperty.Register<AcrylicMaterialExtension, double>(nameof(TintOpacity));
+        public double MaterialOpacity
+        {
+            get => GetValue(MaterialOpacityProperty);
+            set => SetValue(MaterialOpacityProperty, value);
+        }
+        public readonly static StyledProperty<double> MaterialOpacityProperty =
+            AvaloniaProperty.Register<AcrylicMaterialExtension, double>(nameof(MaterialOpacity));
 
-        public AcrylicBackgroundSource AcrylicBackgroundSource { get; set; } = AcrylicBackgroundSource.Digger;
+        public AcrylicBackgroundSource AcrylicBackgroundSource
+        {
+            get => GetValue(AcrylicBackgroundSourceProperty);
+            set => SetValue(AcrylicBackgroundSourceProperty, value);
+        }
+        public readonly static StyledProperty<AcrylicBackgroundSource> AcrylicBackgroundSourceProperty =
+            AvaloniaProperty.Register<AcrylicMaterialExtension, AcrylicBackgroundSource>(nameof(AcrylicBackgroundSource), AcrylicBackgroundSource.Digger);
     }
 }
---
+++ src/Aura.UI/Xaml/BoxShadowExtension.cs
@@ -0,0 +1,72 @@
+﻿using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Media;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Aura.UI.Xaml
+{
+    public class BoxShadowExtension : StyledElement
+    {
+        public object ProvideValue(IServiceProvider serviceProvider)
+        {
+            var bx = new BoxShadow()
+            {
+                IsInset = IsInset,
+                Blur = BlurRadius,
+                Spread = SpreadRadius,
+                Color = ShadowColor,
+                OffsetX = HorizontalLength,
+                OffsetY = VerticalLength
+            };
+            return new BoxShadows(bx);
+        }
+
+        public double HorizontalLength
+        {
+            get => GetValue(HorizontalLengthProperty);
+            set => SetValue(HorizontalLengthProperty, value);
+        }
+        public readonly static StyledProperty<double> HorizontalLengthProperty = 
+            AvaloniaProperty.Register<BoxShadowExtension, double>(nameof(HorizontalLength),0);
+
+        public double VerticalLength
+        {
+            get => GetValue(VerticalLengthProperty);
+            set => SetValue(VerticalLengthProperty, value);
+        }
+        public readonly static StyledProperty<double> VerticalLengthProperty =
+            AvaloniaProperty.Register<BoxShadowExtension, double>(nameof(VerticalLength),0);
+        public double BlurRadius
+        {
+            get => GetValue(BlurRadiusProperty);
+            set => SetValue(BlurRadiusProperty, value);
+        }
+        public readonly static StyledProperty<double> BlurRadiusProperty =
+            AvaloniaProperty.Register<BoxShadowExtension, double>(nameof(BlurRadius),5);
+        public double SpreadRadius
+        {
+            get => GetValue(SpreadRadiusProperty);
+            set => SetValue(SpreadRadiusProperty, value);
+        }
+        public readonly static StyledProperty<double> SpreadRadiusProperty =
+            AvaloniaProperty.Register<BoxShadowExtension, double>(nameof(SpreadRadius),0);
+
+        public Color ShadowColor
+        {
+            get => GetValue(ShadowColorProperty);
+            set => SetValue(ShadowColorProperty, value);
+        }
+        public readonly static StyledProperty<Color> ShadowColorProperty =
+            AvaloniaProperty.Register<BoxShadowExtension, Color>(nameof(ShadowColor));
+
+        public bool IsInset
+        {
+            get => GetValue(IsInsetProperty);
+            set => SetValue(IsInsetProperty, value);
+        }
+        public readonly static StyledProperty<bool> IsInsetProperty =
+            AvaloniaProperty.Register<BoxShadowExtension, bool>(nameof(IsInset), false);
+    }
+}
