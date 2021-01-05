f2a95b0e178a14c4a618a8f3a500a051a6e0ee6a
PieroCastillo <39932324+PieroCastillo@users.noreply.github.com>
2021-01-04 23:37:52 -0500

    main styles file

--- src/Aura.UI/AuraUI.xaml.cs
+++ src/Aura.UI/AuraUI.xaml.cs
@@ -1,4 +1,7 @@
-﻿using Avalonia.Styling;
+﻿using Avalonia.Controls;
+using Avalonia.Controls.Primitives;
+using Avalonia.Media;
+using Avalonia.Styling;
 using System;
 using System.Collections.Generic;
 using System.Text;
@@ -7,5 +10,10 @@ namespace Aura.UI
 {
     public class AuraUITheme : Styles
     {
+        //static AuraUITheme()
+        //{
+        //    TemplatedControl.BackgroundProperty.OverrideDefaultValue<TemplatedControl>(Brushes.Transparent);
+        //    Panel.BackgroundProperty.OverrideDefaultValue<TemplatedControl>(Brushes.Transparent);
+        //}
     }
 }
