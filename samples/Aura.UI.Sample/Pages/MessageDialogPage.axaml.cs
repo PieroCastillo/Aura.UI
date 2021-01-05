731d2ef84d6199c9ec5a1241c4120f1364534c5c
PieroCastillo <39932324+PieroCastillo@users.noreply.github.com>
2021-01-04 23:37:24 -0500

    first page sample

---
+++ samples/Aura.UI.Sample/Pages/MessageDialogPage.axaml.cs
@@ -0,0 +1,28 @@
+﻿using Aura.UI.UIExtensions;
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Interactivity;
+using Avalonia.Markup.Xaml;
+using Aura.UI.Services;
+
+namespace Aura.UI.ControlsGallery.Pages
+{
+    public class MessageDialogPage : UserControl
+    {
+        public MessageDialogPage()
+        {
+            this.InitializeComponent();
+        }
+
+        private void InitializeComponent()
+        {
+            AvaloniaXamlLoader.Load(this);
+        }
+
+        public void ShowMessageDialog(object sender, RoutedEventArgs e)
+        {
+            var win = this.GetParentTOfVisual<Window>();
+            win.NewMessageDialog("Your Message Title","Your Message Content", null);
+        }
+    }
+}
