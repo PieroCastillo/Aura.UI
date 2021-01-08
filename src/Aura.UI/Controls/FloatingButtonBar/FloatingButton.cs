448e440827c0ad71fa676d2c00df2fc3040f81b3
PieroCastillo <39932324+PieroCastillo@users.noreply.github.com>
2021-01-08 00:18:32 -0500

    control like WhatsApp Button Bar

---
+++ src/Aura.UI/Controls/FloatingButtonBar/FloatingButton.cs
@@ -0,0 +1,17 @@
+﻿using Avalonia.Controls;
+using Avalonia.Media;
+using Avalonia.Media.Imaging;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Aura.UI.Controls
+{
+    /// <summary>
+    /// The Content can be an <see cref="IImage"/> only. 
+    /// </summary>
+    public class FloatingButton : Button
+    {
+        
+    }
+}
---
+++ src/Aura.UI/Controls/FloatingButtonBar/FloatingButtonBar.cs
@@ -0,0 +1,44 @@
+﻿using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Media;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Aura.UI.Controls
+{
+    public class FloatingButtonBar : ItemsControl
+    {
+        public bool IsExpanded
+        {
+            get => GetValue(IsExpandedProperty);
+            set => SetValue(IsExpandedProperty, value);
+        }
+        public static readonly StyledProperty<bool> IsExpandedProperty =
+            AvaloniaProperty.Register<FloatingButtonBar, bool>(nameof(IsExpanded), false);
+
+        public FloatingButtonBarExpandDirection ExpandDirection
+        {
+            get => GetValue(ExpandDirectionProperty);
+            set => SetValue(ExpandDirectionProperty, value);
+        }
+        public static readonly StyledProperty<FloatingButtonBarExpandDirection> ExpandDirectionProperty =
+            AvaloniaProperty.Register<FloatingButtonBar, FloatingButtonBarExpandDirection>(nameof(ExpandDirection), FloatingButtonBarExpandDirection.ToRight);
+    
+        public IImage Icon
+        {
+            get => GetValue(IconProperty);
+            set => SetValue(IconProperty, value);
+        }
+        public static readonly StyledProperty<IImage> IconProperty =
+            AvaloniaProperty.Register<FloatingButtonBar, IImage>(nameof(Icon));
+    }
+
+    public enum FloatingButtonBarExpandDirection
+    {
+        ToLeft,
+        ToRight,
+        ToTop,
+        ToBottom
+    }
+}
