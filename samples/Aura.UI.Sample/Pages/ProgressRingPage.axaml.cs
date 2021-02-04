11304722415a35e4efe585f88f4754529e162500
PieroCastillo <pierocastillo2005@gmail.com>
2021-02-03 22:16:14 -0500

    sample app is going to be finished!!!

---
+++ samples/Aura.UI.Sample/Pages/ProgressRingPage.axaml.cs
@@ -0,0 +1,19 @@
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace Aura.UI.ControlsGallery.Pages
+{
+    public class ProgressRingPage : UserControl
+    {
+        public ProgressRingPage()
+        {
+            InitializeComponent();
+        }
+
+        private void InitializeComponent()
+        {
+            AvaloniaXamlLoader.Load(this);
+        }
+    }
+}
