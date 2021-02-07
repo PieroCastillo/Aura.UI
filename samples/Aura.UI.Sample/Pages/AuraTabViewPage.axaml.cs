d12efcb94565c863844e8872900719fd7ac6cc20
PieroCastillo <pierocastillo2005@gmail.com>
2021-02-06 23:53:32 -0500

    update sample

--- samples/Aura.UI.Sample/Pages/AuraTabViewPage.axaml.cs
+++ samples/Aura.UI.Sample/Pages/AuraTabViewPage.axaml.cs
@@ -1,5 +1,7 @@
-﻿using Avalonia;
+﻿using Aura.UI.Controls;
+using Avalonia;
 using Avalonia.Controls;
+using Avalonia.Interactivity;
 using Avalonia.Markup.Xaml;
 
 namespace Aura.UI.ControlsGallery.Pages
@@ -14,6 +16,13 @@ namespace Aura.UI.ControlsGallery.Pages
         private void InitializeComponent()
         {
             AvaloniaXamlLoader.Load(this);
+        }
+
+        public void AddTab(object sender, RoutedEventArgs e)
+        {
+            var atw = sender as AuraTabView;
+
+            atw.AddTab(new AuraTabItem { Header = "Header", Content = new TextBlock { Text = "Content", Margin = new Thickness(10) } });
         }
     }
 }
