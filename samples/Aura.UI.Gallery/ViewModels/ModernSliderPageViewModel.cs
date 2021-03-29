48ae3511b092ec33346125382ce5881135739195
PieroCastillo <pierocastillo2005@gmail.com>
2021-03-29 15:21:01 -0500

    0.1.3

---
+++ samples/Aura.UI.Gallery/ViewModels/ModernSliderPageViewModel.cs
@@ -0,0 +1,27 @@
+﻿using ReactiveUI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aura.UI.Gallery.ViewModels
+{
+    public class ModernSliderPageViewModel : ViewModelBase
+    {
+        public double _horizontalValue;
+        public double _verticalValue;
+
+        public double HorizontalValue
+        {
+            get => _horizontalValue;
+            set => this.RaiseAndSetIfChanged(ref _horizontalValue, value);
+        }
+
+        public double VerticalValue
+        {
+            get => _verticalValue;
+            set => this.RaiseAndSetIfChanged(ref _verticalValue, value);
+        }
+    }
+}
