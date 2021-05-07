b9c676b7fff0d0dbca47d905d24806c6af82da05
Piero Castillo <pierocastillo2005@gmail.com>
2021-05-07 17:16:29 -0500

    backup
    backup

---
+++ src/Aura.UI/Controls/ButtonViewer/ButtonViewer.cs
@@ -0,0 +1,15 @@
+using Avalonia.Controls;
+using Avalonia.Controls.Primitives;
+using Avalonia.Layout;
+
+namespace Aura.UI.Controls.ButtonViewer
+{
+    public class ButtonViewer : ContentControl
+    {
+        public double YOffset;
+        public double XOffset;
+        public Orientation Orientation;
+        
+        
+    }
+}
