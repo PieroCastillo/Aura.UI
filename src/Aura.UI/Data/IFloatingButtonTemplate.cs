67b2429117a7fc0c55a2abcf2dbbee6dcd3c5efb
Piero Castillo <pierocastillo2005@gmail.com>
2021-05-28 10:17:30 -0500

    add fb template

---
+++ src/Aura.UI/Data/IFloatingButtonTemplate.cs
@@ -0,0 +1,16 @@
+﻿using Avalonia.Media;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Input;
+
+namespace Aura.UI.Data
+{
+    public interface IFloatingButtonTemplate
+    {
+        public IImage Icon { get; }
+        public string ToolTip { get; }
+        public ICommand Command { get; }
+        public object CommandParameter { get; }
+    }
+}
