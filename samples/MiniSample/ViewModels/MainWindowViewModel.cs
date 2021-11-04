66f0abc4b700023061fad8883f59d833ac84b443
Piero Castillo <pierocastillo2005@gmail.com>
2021-11-03 22:29:17 -0500

    add miniSample (just for fast control testing)

---
+++ samples/MiniSample/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MiniSample.ViewModels
+{
+    public class MainWindowViewModel : ViewModelBase
+    {
+        public string Greeting => "Welcome to Avalonia!";
+    }
+}
---
+++ samples/MiniSample/ViewModels/ViewModelBase.cs
@@ -0,0 +1,11 @@
+using ReactiveUI;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MiniSample.ViewModels
+{
+    public class ViewModelBase : ReactiveObject
+    {
+    }
+}
