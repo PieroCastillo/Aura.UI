f327e33400d1feb552ef40c44e2cc40e308a7b55
Piero Castillo <pierocastillo2005@gmail.com>
2021-11-12 22:58:33 -0500

    add ext pprops

--- src/Aura.UI/ExtensionProperties/ButtonExts.cs
+++ src/Aura.UI/ExtensionProperties/ButtonExts.cs
@@ -1,7 +1,27 @@
+using Avalonia;
+using Avalonia.Controls;
+using System;
+using AuraUtilities;
+
 namespace Aura.UI.ExtensionProperties
 {
-    public class ButtonExts
+    public class ButtonExts : AvaloniaObject
     {
-        
+        public static readonly AttachedProperty<string> UrlProperty =
+                AvaloniaProperty.RegisterAttached<ButtonExts, Button, string>("Url");
+
+        public static string GetUrl(Button button) => button.GetValue(UrlProperty);
+        public static void SetUrl(Button button, string url) => button.SetValue(UrlProperty, url);
+
+        static ButtonExts()
+        {
+            UrlProperty.Changed.Subscribe(onNext: e =>
+            {
+                if (e.Sender is Button btn && !string.IsNullOrEmpty(GetUrl(btn)))
+                {
+                    UrlUtils.OpenUrl(GetUrl(btn));
+                }
+            });
+        }
     }
 }
